e16a2272f168f3776a12f419e5ccd278bbb81787
unknown <mdtanbir576@gmail.com>
2022-05-02 00:29:48 +0600

    loading all products

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
@@ -10,13 +11,53 @@ app.use(express.json());
 
 const port = 5000;
 
+const uri =
+  "mongodb+srv://spiceuser:KClKK8zwv8Z9dweL@cluster0.u9lhh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 
 app.get("/", (req,res) => {
     res.send("Server is running succesfully. Now connect mongodb to your server")
 })
 
 
-const dbpass = "KClKK8zwv8Z9dweL";
+
+async function run(){
+    try{
+      await client.connect();
+      const productcollection = client.db("spice-store").collection("products");
+
+      app.get("/products", async (req, res) => {
+        const query = {};
+        const cursor = productcollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+      });
+
+      // for single data
+
+    //   app.get("/inventory/:id", async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: ObjectId(id) };
+    //     const product = await productcollection.findOne(query);
+    //     res.send(product);
+    //   });
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+
+
+
+//const dbpass = "KClKK8zwv8Z9dweL";
 
 
 app.listen(port, () => {
